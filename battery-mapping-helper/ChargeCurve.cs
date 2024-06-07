13db6acb069184337727f72bc899562cd64b201a
kutukvpavel <kutukvpavel@PhotoPC>
2024-06-07 21:42:03 +0300

    Добавьте файлы проекта.

---
+++ battery-mapping-helper/ChargeCurve.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battery_mapping_helper
+{
+    public enum ChargeDirection
+    {
+        Discharge,
+        Charge
+    }
+
+    public class UniformVoltageStepChargeCurve
+    {
+        public double StartV { get; set; }
+        public double EndV { get; set; }
+        public double StepV { get; set; }
+        public int RoundingDigitsV { get; set; }
+
+        public double StartFraction { get; set; }
+        public double EndFraction { get; set; }
+
+        public ChargeDirection Direction { get; set; }
+
+        public List<double> CalculateFractions(List<double> knownVolts, List<double> knownFractions)
+        {
+            if (knownVolts.Count != knownFractions.Count) throw new ArgumentOutOfRangeException();
+
+            knownVolts.Sort();
+            knownFractions.Sort();
+            knownVolts[0] = Math.Round(knownVolts[0], RoundingDigitsV);
+            for (int i = 1; i < knownVolts.Count; i++)
+            {
+                knownVolts[i] = Math.Round(knownVolts[i], RoundingDigitsV);
+                double diff = knownVolts[i] - knownVolts[i - 1];
+
+                if (Math.Abs(diff) >= StepV) continue;
+
+                knownVolts.RemoveAt(i);
+                knownFractions.RemoveAt(i);
+                i--;
+            }
+
+            double minKnownVolts = knownVolts.First();
+            double maxKnownVolts = knownVolts.Last();
+            double minKnownFracts = knownFractions.First();
+            double maxKnownFracts = knownFractions.Last();
+
+            List<double> fractions = new((int)((EndV - StartV) / StepV) + 1);
+
+            double currentVolts = StartV;
+            while (currentVolts < minKnownVolts)
+            {
+                fractions.Add(StartFraction + (minKnownFracts - StartFraction) * (currentVolts - StartV) / (minKnownVolts - StartV));
+                currentVolts += StepV;
+            }
+
+            for (int i = 0; i < knownVolts.Count - 1; i++)
+            {
+                double stepStartV = knownVolts[i];
+                double stepEndV = knownVolts[i + 1];
+                double stepStartFract = knownFractions[i];
+                double stepEndFract = knownFractions[i + 1];
+
+                while (currentVolts < stepEndV)
+                {
+                    fractions.Add(stepStartFract + (stepEndFract - stepStartFract) * (currentVolts - stepStartV) / (stepStartV - stepEndV));
+
+                    currentVolts += StepV;
+                }
+            }
+
+            while (currentVolts < EndV)
+            {
+                fractions.Add(EndFraction - (EndFraction - maxKnownFracts) * (EndV - currentVolts) / (EndV - maxKnownVolts));
+                currentVolts += StepV;
+            }
+
+            switch (Direction)
+            {
+                case ChargeDirection.Discharge:
+                    fractions.Reverse();
+                    break;
+                default:
+                    break;
+            }
+
+            return fractions;
+        }
+    }
+}
---
+++ battery-mapping-helper/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace battery_mapping_helper
+{
+    class Program
+    {
+        static void WriteOutputFile(string path, List<double> contents, int roundingDigits)
+        {
+            File.WriteAllLines(Path.Combine(Environment.CurrentDirectory, path), contents.Select(x => x.ToString($"F{roundingDigits}", CultureInfo.InvariantCulture)));
+        }
+
+        static void Main(string[] args)
+        {
+            List<double> chargeVolts = new ();
+            List<double> chargeFracts = new ();
+            List<double> dischargeVolts = new();
+            List<double> dischargeFracts = new();
+
+            var source = File.ReadLines(@"C:\Users\Photo\Documents\LiFePO4_600mAh_mapping.csv").Skip(1);
+            foreach (var line in source)
+            {
+                string[] cells = line.Split(';');
+                if (cells[0].Length > 0 && cells[1].Length > 0)
+                {
+                    dischargeVolts.Add(double.Parse(cells[0], NumberStyles.Float));
+                    dischargeFracts.Add(double.Parse(cells[1], NumberStyles.Float));
+                }
+                if (cells[2].Length > 0 && cells[3].Length > 0)
+                {
+                    chargeVolts.Add(double.Parse(cells[2], NumberStyles.Float));
+                    chargeFracts.Add(double.Parse(cells[3], NumberStyles.Float));
+                }
+            }
+
+            UniformVoltageStepChargeCurve chargeCurve = new()
+            {
+                Direction = ChargeDirection.Charge,
+                StartV = 2.700,
+                EndV = 3.723,
+                StepV = 0.001,
+                StartFraction = 0,
+                EndFraction = 1,
+                RoundingDigitsV = 3
+            };
+            UniformVoltageStepChargeCurve dischargeCurve = new()
+            {
+                Direction = ChargeDirection.Charge,
+                StartV = 2.700,
+                EndV = 3.723,
+                StepV = 0.001,
+                StartFraction = 0,
+                EndFraction = 1,
+                RoundingDigitsV = 3
+            };
+
+            var chargeRes = chargeCurve.CalculateFractions(chargeVolts, chargeFracts).Select(x => x >= 0 ? x : 0).ToList();
+            chargeRes.Sort();
+            var dischargeRes = dischargeCurve.CalculateFractions(dischargeVolts, dischargeFracts).Select(x => x >= 0 ? x : 0).ToList();
+            dischargeRes.Sort();
+
+            WriteOutputFile("charge.csv", chargeRes, 4);
+            WriteOutputFile("discharge.csv", dischargeRes, 4);
+        }
+    }
+}
